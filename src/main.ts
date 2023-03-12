db5e4a85cbc2c861c6fb10db56065c37326946b4
mtagizadee <magatagi0@gmail.com>
2023-03-12 19:45:43 +0400

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  return app;
+}
+
+async function start() {
+  const PORT = process.env.PORT || 3000;
+  const app = await bootstrap();
+
+  await app.listen(PORT, () => console.log(`SERVER STARTED ON PORT: ${PORT}`));
+}
+
+start().catch((error) => console.error(error));
